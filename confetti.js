727e58c9e062e24ae2d81c1dcb0f77ed58c01f92
shinx14 <carol.w.2090@gmail.com>
2024-06-07 14:34:03 -0400

    commit
    trying to make a page

---
+++ confetti.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Function to generate continuous confetti from bottom to top
+    function startConfetti() {
+        confetti({
+            particleCount: 15,
+            angle: 90, // Angle set to 90 degrees to move downwards
+            spread: 1000,
+            origin: { y: 0 }, // Start from the top of the page
+            colors: ['#bb0000', '#ffffff'] // Custom colors for the confetti
+        });
+
+        setTimeout(startConfetti, 250); 
+    }
+
+    // Start the confetti effect
+    startConfetti();
+});
