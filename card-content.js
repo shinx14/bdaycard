6ad65b1752805be0b606709f2f84e46d7c8ada68
shinx14 <carol.w.2090@gmail.com>
2024-06-07 16:11:18 -0400

    commt 7
    fixed spelling and other errors

--- card-content.js
+++ card-content.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const sentences = ["you are my favorite cheese", "you still wouldve been mine we wouldve been timeless", "youre the sweetest corn", 
+  const sentences = ["youre my favorite cheese", "you still wouldve been mine we wouldve been timeless", "youre the sweetest corn", 
     "youre the coolest poet ever", "youre the best outlaw who stole my heart", "i just wanna be yours", "one single thread of gold tied me to you <3"];
   const randomWord = sentences[Math.floor(Math.random() * sentences.length)];
   document.getElementById("text-container").textContent = randomWord;
